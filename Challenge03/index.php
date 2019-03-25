040c6fd059e995b24108f16c8c65a1bd50b8e3da
k <48893194+k@users.noreply.github.com>
2019-03-24 19:32:28 -0500

    Update index.php
    fixed the header already being sent issue on challenge 3

--- Challenge03/index.php
+++ Challenge03/index.php
@@ -1,11 +1,3 @@
-<html>
-<head></head>
-<body>
-You must get exactly 100,000 points to get the flag!
-</br><br/>
-<form method=post>
-<button type=submit name='submit'>Add Point!</button>
-</form>
 <?php
 require_once "functions.php";
 include_once "flag3.php"; 
@@ -28,21 +20,31 @@ update_cookie($value);
 
 function new_cookie(){
 	setcookie("points",0,time() + (86400 * 30),'/');	
-	write_table(0);
+	write_page(0);
 	
 }
 function update_cookie($v){
 	$new_value=encode_cookie(decode_cookie($v)+1);
 	setcookie("points",$new_value,time() + (86400 * 30),'/');
-	write_table(decode_cookie($new_value));
+	write_page(decode_cookie($new_value));
 }
 
-function write_table($v){
-echo "<div>
+function write_page($v){
+echo "
+<html>
+<head></head>
+<body>
+You must get exactly 100,000 points to get the flag!
+</br><br/>
+<form method=post>
+<button type=submit name='submit'>Add Point!</button>
+</form>
+<div>
 <table border='1'><tr><td>Points:</td><td>".$v."</td></td></table>
-</div>";	
+</div>
+</body>
+</html>";	
 }
 ?>
 
-</body>
-</html>
+
