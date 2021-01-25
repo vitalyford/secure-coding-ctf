7e7b25e283481a8c0a1847528d7aa445d605c76f
Curzuv <alexandr.chebatarev@gmail.com>
2021-01-24 20:59:46 -0500

    updated week 4 template

--- Challenges/Challenge05/index.php
+++ Challenges/Challenge05/index.php
@@ -22,7 +22,32 @@
 
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+    <style>
+      * {
+   box-sizing: border-box;
+  }
+  *:before,
+  *:after {
+   box-sizing: border-box;
+  }
+  html,
+  body {
+   height: 100%;
+   position: relative;
+  }
+  #main-container {
+   min-height: 100vh; /* will cover the 100% of viewport */
+   overflow: hidden;
+   display: block;
+   position: relative;
+   padding-bottom: 100px; /* height of your footer */
+  }
+  footer {
+   position: absolute;
+   bottom: 0;
+   width: 100%;
+  }
+    </style>
   </head>
 
   <body data-gr-c-s-loaded="true">
@@ -35,26 +60,26 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="https://twitter.com/CyberStudents" target="_blank">Twitter</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.linkedin.com/groups/8477102//" target="_blank">Linkedin</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.facebook.com/CyberStudents.org/s" target="_blank">Facebook</a>
-            </li>
-          </ul>
+      <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="https://twitter.com/CyberStudents" target="_blank">Twitter</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.linkedin.com/groups/8477102//" target="_blank">Linkedin</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.facebook.com/CyberStudents.org/s" target="_blank">Facebook</a>
+          </li>
+        </ul>
         </div>
     </div>
   </nav>
 
-    <header class="masthead text-center" style="padding-top: 6.5em; padding-bottom: 2.5em;">
+  <div id="main-container">
+    <header class="masthead text-center text-white" style="padding-top: 6.5em; padding-bottom: 2.5em;"></header>
       <div class="masthead-content">
         <div class="container">
-          <h1 class="masthead-heading mb-0" style="color: #231F20;">Secure Coding CTF - The Winter Challenge</h1>
-          <!-- <h2 class="masthead-subheading mb-0"></h2> -->
+          <h1 class="masthead-heading mb-0" style="text-align:center;">Secure Coding CTF - The Winter Challenge</h1>
         </div>
       </div>
     </header>
@@ -64,7 +89,6 @@
         <div class="row align-items-center">
           <div class="col-lg-4 order-lg-2">
             <div class="p-5">
-              <img class="img-fluid rounded-circle" src="https://raw.githubusercontent.com/StartBootstrap/startbootstrap-one-page-wonder/master/img/01.jpg" alt="">
               <blockquote style="padding-top: 20px;">
                 <p>Good code is its own best documentation.</p>
                 <footer>&mdash; <cite>Steve McConnell</cite></footer>
@@ -115,14 +139,14 @@
     </section>
 
     <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Steal The Steal CTF&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-dragon"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Because We Care</p>
-        <p class="m-0 text-center text-white">Developed by<br>Amela Gjishti | Daniel Tyler | Konstantin Menako | Vitaly Ford</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
+    <footer class="py-5">
+    <div class="container">
+      <p class="m-0 text-center">Secure Coding CTF - The Winter Challenge&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-dragon"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Because We Care</p>
+      <p class="m-0 text-center">Deployed by<br>Vitaly Ford && Alexandr Chebatarev</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+            </div>
     <!-- Bootstrap core JavaScript -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
